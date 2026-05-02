ba91b523f2519657665796e8f797bb80112e7d69
souvik27-lab <158319175+souvik27-lab@users.noreply.github.com>
2026-05-02 22:13:48 +0530

    Update DBConnection.java

--- src/main/java/com/booknest/util/DBConnection.java
+++ src/main/java/com/booknest/util/DBConnection.java
@@ -5,36 +5,25 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static Connection con;
-
     public static Connection getConnection() {
+
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-            if (con == null || con.isClosed()) {
+            String host = System.getenv("MYSQLHOST");
+            String port = System.getenv("MYSQLPORT");
+            String db   = System.getenv("MYSQLDATABASE");
+            String user = System.getenv("MYSQLUSER");
+            String pass = System.getenv("MYSQLPASSWORD");
 
-                Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + db +
+                    "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
-                String host = System.getenv("MYSQLHOST");
-                String port = System.getenv("MYSQLPORT");
-                String db = System.getenv("MYSQLDATABASE");
-                String user = System.getenv("MYSQLUSER");
-                String pass = System.getenv("MYSQLPASSWORD");
-
-                System.out.println("HOST = " + host);
-                System.out.println("PORT = " + port);
-                System.out.println("DB = " + db);
-                System.out.println("USER = " + user);
-
-                String url = "jdbc:mysql://" + host + ":" + port + "/" + db +
-                        "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-
-                con = DriverManager.getConnection(url, user, pass);
-            }
+            return DriverManager.getConnection(url, user, pass);
 
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-
-        return con;
     }
 }
